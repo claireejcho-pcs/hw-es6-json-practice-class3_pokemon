52778c9327321802d752047d91beea70d179514f
Glitch (hello-webpage) <none>
2021-02-04 20:01:59 +0000

    🐷⏰ Checkpoint ./script.js:17397933/215 ./.glitch-assets:17397933/1849 ./style.css:17397933/72 ./index.html:17397933/89

--- script.js
+++ script.js
@@ -9,6 +9,9 @@ clickMe.addEventListener("click", writeStory, false);
 
 function writeStory() {
   let launchedStory = "<p>Welcome, " + myName.value + ".</p>";
-
+  
+ /* 
+ launchedStory += "<div><img src='https://cdn.glitch.com/ee45a6b7-03f1-42da-ab47-5ff127acfc62%2F"+myName.value+".png?v=1612468365638'></div>";
+*/
   greeting.innerHTML = launchedStory;
 }
