972b93c22692edb6076e613e01024951a4af2417
Glitch (hello-webpage) <none>
2021-02-04 19:41:44 +0000

    🎋🐉 Checkpoint ./index.html:17397933/300 ./script.js:17397933/1855

--- script.js
+++ script.js
@@ -1,36 +1,14 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-// console.log("hi");
+let myName = document.querySelector("#my-name");
+let greeting = document.querySelector("#greeting");
+let clickMe = document.querySelector("#click-me");
 
-
-let nombre = document.querySelector("#nombre");
-let color = document.querySelector("#color");
-let place = document.querySelector("#place");
-let number = document.querySelector("#number");
-let story = document.querySelector("#story");
-let launch = document.querySelector("#launch");
-let result = document.querySelector("#result");
-launch.addEventListener("click", writeStory, false);
+clickMe.addEventListener("click", writeStory, false);
 
 function writeStory() {
-  let launchedStory = "";
-  launchedStory += "<p>Welcome, " + nombre.value + ".</p>";
-  launchedStory += "<p>Your color is: " + color.value + "</p>";
-  launchedStory += "<p>Your place is: " + place.value + ".</p>";
-  launchedStory += "<p>Your favorite number is: " + number.value + ".</p>";
+  let launchedStory = "<p>Welcome, " + myName.value + ".</p>";
 
-  story.innerHTML = launchedStory;
-
-  if (color.value == "green") {
-    result.innerHTML = "How basic.";
-  } 
-  else if (color.value == "red"){
-    result.innerHTML = "How bold.";
-    
-  }
-  else {
-    result.innerHTML = "How wonderful.";
-  }
+  greeting.innerHTML = launchedStory;
 }
