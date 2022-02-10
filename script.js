386812977914d14bae52b9f960a2ffc7bf76742d
Glitch (hello-webpage) <none>
2022-02-10 15:39:25 +0000

    🤑🏞 Checkpoint ./script.js:17397933/524

--- script.js
+++ script.js
@@ -7,19 +7,25 @@ let clickMe = document.querySelector("#click-me"); // button
 let myNumber = document.querySelector("#my-number");
 clickMe.addEventListener("click", writeStory, false);
 
-console.log(typeof(myNumber));
+//console.log(typeof(myNumber));
 
 function random(number) { 
 parseFloat(number);  
 return Math.floor(Math.random() * number); 
 }
 
-let rando = random(myNumber.value);
+//console.log(typeof(rando));
+//console.log(rando);
 
 function writeStory() {
+  //moved this here because myNumber doesn't have a value to read until the button is clicked
+  let rando = random(myNumber.value);
+  let nameLength = myName.value.length;
+    console.log(nameLength);
+  let randoNameNumber = random(nameLength);
   let launchedStory = "<p>Welcome, " + myName.value + ".</p>";
   launchedStory += "<p>Your favorite number is, " + rando + ".</p>"
- 
+  launchedStory += "<p>Or maybe it's " + randoNameNumber + ".</p>"
  launchedStory =  launchedStory + "<div><img src='https://cdn.glitch.com/ee45a6b7-03f1-42da-ab47-5ff127acfc62%2F"+myName.value+".png?v=1612468365638'></div>";
 
   greeting.innerHTML = launchedStory;
